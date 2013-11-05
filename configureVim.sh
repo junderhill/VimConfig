2c41dfdad36032835a1f0e6a69e40238eeaacb71
jason <jay.underhill@gmail.com>
2013-11-05 23:08:17 +0000

    check for git and curl

--- configureVim.sh
+++ configureVim.sh
@@ -4,6 +4,9 @@ mkdir ~/.vim
 mkdir ~/.vim/bundle
 mkdir ~/.vim/autoload
 
+#Check if curl is installed
+command -v curl >/dev/null 2>&1 || { echo "I require curl but it's not installed. Aborting." >&2; exit 1; }
+
 #Install pathogen
 curl -Sso ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
@@ -13,6 +16,9 @@ if [-f ~/.vimrc ]; then
   cp ~/.vimrc ~/.vimrc_backup
   rm ~/.vimrc
 fi
+
+#Check git is installed
+command -v git >/dev/null 2>&1 || { echo "I require git but it's not installed. Aborting." >&2; exit 1; }
 
 #Get vimrc file
 cd ~
