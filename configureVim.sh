6047a577e08eb9c02a851d625ba7315e14e31ba8
Jason Underhill <jason@junderhill.com>
2022-10-13 11:45:09 +0100

    Add new vimrc file (generated by vim bootstrap), added some tweaks from my existing vimrc file

--- configureVim.sh
+++ configureVim.sh
@@ -5,10 +5,6 @@ mkdir ~/.vim
 #Check if curl is installed
 command -v curl >/dev/null 2>&1 || { echo "I require curl but it's not installed. Aborting." >&2; exit 1; }
 
-#Install pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle; \
-  curl -LSso ~/.vim/autoload/pathogen.vim \
-  https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 
 #Backup vimrc
 
@@ -21,21 +17,20 @@ fi
 #Check git is installed
 command -v git >/dev/null 2>&1 || { echo "I require git but it's not installed. Aborting." >&2; exit 1; }
 
-cp vimrc ~/.vimrc
+cp newvimrc ~/.vimrc
 
 #get plugins
-cd ~/.vim/bundle
 
-git clone https://github.com/kien/ctrlp.vim.git
-git clone https://github.com/nathanaelkane/vim-indent-guides.git
-git clone https://github.com/plasticboy/vim-markdown.git
+# git clone https://github.com/kien/ctrlp.vim.git
+# git clone https://github.com/nathanaelkane/vim-indent-guides.git
+# git clone https://github.com/plasticboy/vim-markdown.git
 
-git clone https://github.com/preservim/nerdtree.git
+# git clone https://github.com/preservim/nerdtree.git
 
-#async linting engine
-git clone https://github.com/w0rp/ale.git
+# #async linting engine
+# git clone https://github.com/w0rp/ale.git
 
-git clone https://github.com/OmniSharp/omnisharp-vim.git 
-#mappings and integrations for omnisharp
-git clone https://github.com/nickspoons/vim-sharpenup.git
-git clone git@github.com:prabirshrestha/asyncomplete.vim.git
+# git clone https://github.com/OmniSharp/omnisharp-vim.git 
+# #mappings and integrations for omnisharp
+# git clone https://github.com/nickspoons/vim-sharpenup.git
+# git clone git@github.com:prabirshrestha/asyncomplete.vim.git
